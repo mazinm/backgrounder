a53d57aa62ca47f3a7a10c236ca049ccb888bec9
Mazin Melegy <mazin.melegy@gmail.com>
2014-01-09 21:31:45 +0000

    about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Backgrounder::Application.routes.draw do
   root to: 'pages#index'
   
+  get "/pages/about" => 'pages#about', :as => 'about'
+  
   get "/sessions/gettweets" => 'sessions#gettweets', :as => 'tweets'
   get "/sessions/findhashtags" => 'sessions#findhashtags', :as => 'hashtags'
   get "/sessions/providebackground" => 'sessions#providebackground', :as => 'backgrounder'
