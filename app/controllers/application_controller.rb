6e03eace1d9c017e0f9cf4667a77490c8a3b3300
Mazin Melegy <mazin.melegy@gmail.com>
2014-01-09 15:55:20 +0000

    mobile styles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,9 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+  
+  def mobile_device?
+    request.user_agent =~ /Mobile|webOS/
+  end
+  helper_method :mobile_device?
 end
